4433941ac5f21fa7d2d5d205f00795c51f069d37
yinqingjiang <Qingjiang.yin@stonybrook.edu>
2017-07-15 17:20:10 -0500

    add route polyline

--- app/src/main/java/com/schoolbus/schoolbustracking/fragments/home/LocationFragment.java
+++ app/src/main/java/com/schoolbus/schoolbustracking/fragments/home/LocationFragment.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,6 +17,8 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.schoolbus.schoolbustracking.R;
 
 import static com.schoolbus.schoolbustracking.R.id.my_map;
@@ -28,32 +28,13 @@ import static com.schoolbus.schoolbustracking.R.id.my_map;
  */
 
 public class LocationFragment extends Fragment implements OnMapReadyCallback {
-    private ImageView iv_plus;
-    private ImageView iv_minus;
+
     private GoogleMap mMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
      View view=inflater.inflate(R.layout.fragment_location,container,false);
-        iv_plus=view.findViewById(R.id.plus);
-        iv_minus=view.findViewById(R.id.minus);
-        // zoom in listener
-        iv_plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"zoom in",Toast.LENGTH_LONG).show();
-                CameraUpdate zoom =CameraUpdateFactory.zoomIn();
-                mMap.animateCamera(zoom);
-            }
-        });
-        // zoom out listner
-        iv_minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CameraUpdateFactory.zoomOut();
 
-            }
-        });
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(my_map);
         mapFragment.getMapAsync(this);
@@ -62,26 +43,41 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-        LatLng sydney = new LatLng(12.91697, 77.614085);
-        //LatLng sydney = new LatLng(41.9797770, -88.5337430);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Office")) ;
+        // bus stops
+        LatLng start = new LatLng(12.916514, 77.619664);
+        LatLng end = new LatLng(12.916514, 77.604332);
+        LatLng CityBank=new LatLng(12.91697,77.614085);
+        LatLng Lara=new LatLng(12.916274,77.611081);
+        LatLng BTM=new LatLng(12.916535,77.606800);
+        // add markers to the map
+        googleMap.addMarker(new MarkerOptions().position(start)
+                .title("Start Stop")) ;
+        googleMap.addMarker(new MarkerOptions().position(end)
+                .title("End Stop")) ;
+        googleMap.addMarker(new MarkerOptions().position(CityBank)
+                .title("City Bank Stop")) ;
+        googleMap.addMarker(new MarkerOptions().position(Lara)
+                .title("Lara Technologies"));
+        googleMap.addMarker(new MarkerOptions().position(BTM)
+                .title("BTM Water Tank"));
+        // add route to the map
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(start, CityBank, Lara, BTM, end));
+        polyline1.setTag("Route 1");
+        polyline1.setColor(Color.parseColor("#3F51B5"));
         //setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
 //      googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         // marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
         Circle circle = googleMap.addCircle(new CircleOptions()
-                .center(sydney)
+                .center(start)
                 .radius(500)
                 .strokeColor(Color.parseColor("#4fb2ff"))
                 .fillColor(Color.parseColor("#BBDEFB")));
-
         CameraUpdate center =
-                CameraUpdateFactory.newLatLng(sydney);
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(12);
-
+                CameraUpdateFactory.newLatLng(start);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(14);
         googleMap.moveCamera(center);
         googleMap.animateCamera(zoom);
-
     }
 }
