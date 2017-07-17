1eec3d9bcd95b5830621e4878ac1f22974f5c263
yinqingjiang <Qingjiang.yin@stonybrook.edu>
2017-07-16 23:56:34 -0500

    update bus marker and splash

--- app/src/main/java/com/schoolbus/schoolbustracking/fragments/home/LocationFragment.java
+++ app/src/main/java/com/schoolbus/schoolbustracking/fragments/home/LocationFragment.java
@@ -14,8 +14,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -45,16 +43,21 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // bus stops
+        LatLng bus=new LatLng(12.916514,77.616404);
         LatLng start = new LatLng(12.916514, 77.619664);
         LatLng end = new LatLng(12.916514, 77.604332);
         LatLng CityBank=new LatLng(12.91697,77.614085);
         LatLng Lara=new LatLng(12.916274,77.611081);
         LatLng BTM=new LatLng(12.916535,77.606800);
         // add markers to the map
+        googleMap.addMarker(new MarkerOptions().position(bus)
+                .title("Bus")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.bus_24));
+        googleMap.addMarker(new MarkerOptions().position(start)
+                .title("Start Stop")) ;
         googleMap.addMarker(new MarkerOptions().position(start)
                 .title("Start Stop")) ;
         googleMap.addMarker(new MarkerOptions().position(end)
-                .title("End Stop")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.bus_24)); ;
+                .title("End Stop")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.school)); ;
         googleMap.addMarker(new MarkerOptions().position(CityBank)
                 .title("City Bank Stop")) ;
         googleMap.addMarker(new MarkerOptions().position(Lara)
@@ -70,11 +73,11 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         //setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
 //      googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         // marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
-        Circle circle = googleMap.addCircle(new CircleOptions()
-                .center(start)
-                .radius(500)
-                .strokeColor(Color.parseColor("#4fb2ff"))
-                .fillColor(Color.parseColor("#BBDEFB")));
+//        Circle circle = googleMap.addCircle(new CircleOptions()
+//                .center(start)
+//                .radius(500)
+//                .strokeColor(Color.parseColor("#4fb2ff"))
+//                .fillColor(Color.parseColor("#BBDEFB")));
         CameraUpdate center =
                 CameraUpdateFactory.newLatLng(start);
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(13);
